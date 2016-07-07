b5c1fc9a7f367d5430e23468ba153107a3a79a42
Aybou <abou.bacarkercodebysimplon@gmail.com>
2016-07-07 09:12:02 +0200

    modif client

--- app/client/client.ts
+++ app/client/client.ts
@@ -6,13 +6,19 @@ export class Client {
 	nom: string;
 	prenom: string;
 	age: number;
+	nombre_enfant: number;
+	profession: string;
+	adresse:string;
 	caution: number;
 	
-	constructor(id: number, nom: string, prenom: string, age: number, caution: number) {
+	constructor(id: number, nom: string, prenom: string, age: number,nombre_enfant:number, profession: string,adresse:string, caution: number) {
 		this.id = id;
 		this.nom = nom;
 		this.prenom = prenom;
 		this.age = age;
+		this.nombre_enfant = nombre_enfant;
+		this.profession = profession;
+		this.adresse = adresse;
 		this.caution = caution;
 	}
 }
